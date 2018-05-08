a0c37ce88d5a6b0931ad2f950af281962363e07e
Diego Hernandes <diego@hernandev.com>
2018-05-08 04:59:38 -0300

    invert loading order

--- routes/web.php
+++ routes/web.php
@@ -9,8 +9,9 @@ return function (Router $router) {
         return response()->json(["ping" => "pong"]);
     });
 
-    // avatar default route.
-    $router->get('/a/{username}', 'AvatarController@show');
     // alias route for image extension (compat mode).
     $router->get('/a/{username}.{any}', 'AvatarController@show');
+    // avatar default route.
+    $router->get('/a/{username}', 'AvatarController@show');
+
 };
